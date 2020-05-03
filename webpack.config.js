de5ec7bdc2e6df68e249b623098347db38e68796
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-05-03 20:46:25 +0300

    feat: add babel, webpack, eslitn. create index.html, index,js , style.css files

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './index.js',
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: '/build/'
+    },
+    module: {
+        rules: [{
+            test: /\.js/,
+            use: [{
+                loader: 'babel-loader',
+                options: { presets: ["@babel/preset-env"] }
+            }]
+        }]
+    }
+
+}
