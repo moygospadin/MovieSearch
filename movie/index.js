cf9b489c61cb3018577d06e94f9aaa621db32875
moygospadin <60549587+moygospadin@users.noreply.github.com>
2020-05-09 09:10:15 +0300

    feat: optimized requests

--- movie/index.js
+++ movie/index.js
@@ -1,6 +1,6 @@
 "use strict";
-//var apikey = "ef4cf4c7";
-var apikey = "5c527664";
+var apikey = "ef4cf4c7";
+//var apikey = "5c527664";
 import './style.css';
 
 window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
@@ -9,7 +9,6 @@ recognition.interimResults = false;
 
 class Model {
     constructor(options) {
-        console.log("опции", options);
 
         if (options.Poster == "N/A")
             this.img = "./img/no_img.png"
@@ -40,42 +39,28 @@ class Model {
 
 
 
-async function getInfo(id) {
 
-    var url = "https://www.omdbapi.com/?apikey=" + apikey + "&i=" + id;
-    let response = await fetch(url);
-    let data = await response.json();
-
-    return data;
-
-
-}
 const spinner = document.getElementsByClassName('spinner-border')[0];
+
+var i = 1;
+var mov;
+var totalResults;
 
 function getUrl(movie) {
     spinner.classList.remove('d-none');
     var url = "https://www.omdbapi.com/?apikey=" + apikey + "&s=" + movie + "&page=";
-    var movieAmountPage = 0;
-    fetch(url + 1)
+    fetch(url + i)
         .then((response) => {
             return response.json();
         })
         .then((data) => {
-            console.log(data);
-
-            movieAmountPage = Math.floor(data.totalResults / 10) + 1;
-
-            if (data.totalResults > 250) { alert("To many results");
-                spinner.classList.add('d-none'); } else
-            if (!movieAmountPage) {
-                alert(data.Error);
-                spinner.classList.add('d-none');
-            } else if (movieAmountPage > 70) {
-                alert('To many results');
+            if (data.Error == "Movie not found!") {
+                alert('Movie not found!');
                 spinner.classList.add('d-none');
             } else {
-                console.log('srabotalo');
-                idCard(movieAmountPage, url);
+                totalResults = data.totalResults;
+                data.Search.forEach(el => getInfo(el.imdbID));
+                spinner.classList.add('d-none');
             }
         }).catch((e) => {
             console.log("Ошибка");
@@ -83,52 +68,34 @@ function getUrl(movie) {
 
         });
 
+
 }
 
-var movieData = [];
-async function idCard(movieAmountPage, url) {
-    movieData = [];
-    console.log(movieAmountPage);
-    if (movieAmountPage == 1) movieAmountPage++;
-    for (let page = 1; movieAmountPage > page; page++) {
-        console.log('big');
+async function getInfo(id) {
+    var url = "https://www.omdbapi.com/?apikey=" + apikey + "&i=" + id;
+    let response = await fetch(url);
+    let data = await response.json();
+    let a = new Model(data);
+    a.showResult();
 
-        let response = await fetch(url + page);
-        let data = await response.json();
-        data.Search.forEach(el => {
-            movieData.push(el.imdbID);
-        })
-    }
-    mySwiper.removeAllSlides();
-    console.log(movieData);
-
-    cardShow(movieData, 1);
 }
 
-async function cardShow(movieData, page) {
-    for (let i = page - 1; i < page + 12; i++) {
-        if (i < movieData.length) {
-            let param = await getInfo(movieData[i])
-            let a = new Model(param);
-            a.showResult();
-        }
-    }
-    spinner.classList.add('d-none');
-}
 
 var controller = function controller() {
+
     var movie = document.getElementById('movie').value;
     var urlYandex = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200508T173205Z.ad231a2cd00028de.5caad0bb3d04c85a53c6ede494131fe80e58bfd6&text=${movie}&lang=ru-en`;
-
     fetch(urlYandex)
         .then(response => response.json())
         .then(date => date.text.join(''))
         .then((movie) => {
+            mov = movie;
+            mySwiper.removeAllSlides();
             getUrl(movie);
             document.getElementsByClassName('info')[0].innerText = `Showing results for ${movie}`;
         }).catch(e => {
             console.log(e);
-            alert("Неверный запрос")
+            alert("Неверный запрос");
 
         })
 
@@ -185,31 +152,26 @@ var mySwiper = new Swiper('.swiper-container', {
 var page = 0;
 
 mySwiper.on('slideChange', function() {
+    console.log(mySwiper.realIndex);
+    console.log(mySwiper.realIndex % 10 == 7);
 
-    if (mySwiper.slides.length - mySwiper.realIndex <= 5) {
-        page += 12;
-        cardShow(movieData, page);
+    if (mySwiper.realIndex % 10 == 7 && mySwiper.realIndex + 3 < totalResults) {
+        i++;
+        getUrl(mov);
     }
-
 });
 
 import Keyboard from 'rss-virtual-keyboard';
 const keyboard = document.getElementsByClassName('keyboard-img')[0];
 
-
 var kb = new Keyboard().init('.form-control', '.keyboard-container');
 
-
 keyboard.addEventListener("mousedown", () => {
-
     document.getElementsByClassName('keyboard-container')[0].classList.toggle('kb-block');
     if (document.getElementsByClassName('keyboard-container')[0].classList.value == "keyboard-container") {
         document.getElementsByClassName('keyboard-container')[0].innerHTML = "";
     } else {
-
         kb.generateLayout();
-
-
     }
 });
 
@@ -242,12 +204,15 @@ runOnKeys(
 document.querySelector('.speak-btn').addEventListener('click', () => {
     document.querySelector('#movie').value = "";
     document.querySelector('.speak-btn').classList.add('pulse');
+    console.log(recognition);
+
     recognition.start();
 });
 
 recognition.addEventListener('result', event => {
-        let transcript = Array.from(event.results).map((result) => result[0]).map((result) => result.transcript).join('');
-        document.querySelector('#movie').value = transcript;
-        document.querySelector('.speak-btn').classList.remove('pulse');
-    })
-    /*end*/
+    let transcript = Array.from(event.results).map((result) => result[0]).map((result) => result.transcript).join('');
+    document.querySelector('#movie').value = transcript;
+    document.querySelector('.speak-btn').classList.remove('pulse');
+});
+recognition.addEventListener('end', () => document.querySelector('.speak-btn').classList.remove('pulse'));
+/*end*/
